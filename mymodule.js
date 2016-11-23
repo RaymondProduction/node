f7c8b381e87ea8a31dbcd2719dbb487fb8eb35a0
Raymond Perminov <raymondproduction@ukr.net>
2016-11-23 14:27:39 +0200

    Task 6 of learnyounode with correction

--- mymodule.js
+++ mymodule.js
@@ -1,10 +1,15 @@
 const path = require('path');
 
-module.exports = function (arg1,arg2) {
+mymodule.exports = function (arg1,arg2,callback) {
 var fs = require('fs');
-fs.readdir(arg1, function(err, list) {
+fs.readdir(arg1, foo(function(err, list) {
+     
+    if (err)   return callback(err);
+    callback(null,list);
+    /* 
     list.forEach(function(name, i, list) {
       if (path.extname(name)=='.'+arg2) console.log(name);
     });
-});
+   */
+}));
 } 
