89ba37fe5246f8c8c8d55714ac5d14ce1ecb87dd
Raymond Perminov <raymondproduction@ukr.net>
2016-11-23 14:47:47 +0200

    Task 6 final correction

--- make_it_modular.js
+++ make_it_modular.js
@@ -1,17 +1,10 @@
 var mymodule = require('./mymodule.js') ;
 
 const path = require('path');
-/*
-var fs = require('fs');
-fs.readdir(process.argv[2], function(err, list) {
-    list.forEach(function(name, i, list) {
-      if (path.extname(name)=='.'+process.argv[3]) console.log(name);
-    });
-});
-*/
 
-mymodule.exports(process.argv[2],process.argv[3],function(err, list) {
-    list.forEach(function(name, i, list) {
+mymodule(process.argv[2],process.argv[3],function(err, list) {
+   list.forEach(function(name, i, list) {
       if (path.extname(name)=='.'+process.argv[3]) console.log(name);
    });
  });
+
--- mymodule.js
+++ mymodule.js
@@ -1,15 +1,7 @@
-const path = require('path');
-
-mymodule.exports = function (arg1,arg2,callback) {
+module.exports = function (arg1,arg2,callback) {
 var fs = require('fs');
-fs.readdir(arg1, foo(function(err, list) {
-     
+fs.readdir(arg1, function(err, list) { 
     if (err)   return callback(err);
-    callback(null,list);
-    /* 
-    list.forEach(function(name, i, list) {
-      if (path.extname(name)=='.'+arg2) console.log(name);
-    });
-   */
-}));
+     callback(null,list);
+});
 } 
